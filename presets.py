baad90b9cb9e6f32c4ed6c5975f70304fc9296dc
Myudi422 <43848105+Myudi422@users.noreply.github.com>
2021-09-01 22:07:21 +0700

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,10 +1,11 @@
 class Presets(object):
     WELCOME_MESSAGE = """
 <b>Hello.. </b>{}
-<code>I'm an official telecaster bot
-maintained by</code> <b>{}</b>
-<code>Keep me unblocked to receive ma official updates.</code>
+<code>Terimakasih sudah menggunakan bot kami😁</code>
+<code>Silahkan ketik perintah /help untuk Tutorial menggunakan bot ini..</code>
+[List Anime Batch](buttonurl://https://t.me/ccgnimeX/131)
+[Request & Lapor](buttonurl://t.me/CSccgnimeX_bot)
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
-    WAIT_MSG = "<b>Please Wait...</b>"
+    WAIT_MSG = "<b>tunggu bentar...</b>"
     REPLY_ERROR = "<code>Use this command as a replay to any telegram message with out any spaces.</code>"
