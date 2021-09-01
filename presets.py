0849453d538e4dbbc9b665463d602b496f20248a
Myudi422 <43848105+Myudi422@users.noreply.github.com>
2021-09-01 23:06:53 +0700

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,11 +1,4 @@
 class Presets(object):
-    WELCOME_MESSAGE = """
-<b>Hello.. </b>{}
-<code>Terimakasih sudah menggunakan bot kami😁</code>
-<code>Silahkan ketik perintah /help untuk Tutorial menggunakan bot ini..</code>
-[List Anime Batch](buttonurl://https://t.me/ccgnimeX/131)
-[Request & Lapor](buttonurl://t.me/CSccgnimeX_bot)
-    """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>tunggu bentar...</b>"
     REPLY_ERROR = "<code>Use this command as a replay to any telegram message with out any spaces.</code>"
